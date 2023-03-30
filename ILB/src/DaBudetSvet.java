36f2e01ed4df5869f1b2577590bcb734fc1c0baf
Iuliia Kuznetcova <iuliia_kuznetcova@hotmail.com>
2023-03-30 21:26:44 +0200

    commit from Iuliia 30.03.23

---
+++ ILB/src/DaBudetSvet.java
@@ -0,0 +1,13 @@
+package ILB.src;public class DaBudetSvet {
+
+    public static void main(String[] args) {
+        System.out.println("Da budet svet!");
+    }
+}
+
+
+/*
+    detected dubious ownership in repository at '//192.168.2.120/fokuz/QA/ILB-'
+        To add an exception for this directory, call:
+        git config --global --add safe.directory '%(prefix)///192.168.2.120/fokuz/QA/ILB-'
+        Set the environment variable GIT_TEST_DEBUG_UNSAFE_DIRECTORIES=true and run again for more information.*/
--- ILB/src/Helloworld.java
+++ ILB/src/Helloworld.java
@@ -3,5 +3,8 @@ package ILB.src;
 public class Helloworld {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("I vam dobroe utro");
+
+        
     }
 }
